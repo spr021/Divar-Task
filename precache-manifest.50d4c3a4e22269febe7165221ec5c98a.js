5f70583ff86082ea550687cd4eabac229f17d263
Saber Pourrahimi <saber@Sabers-MacBook-Pro.local>
2021-10-16 03:44:59 +0330

    Updates

---
+++ precache-manifest.50d4c3a4e22269febe7165221ec5c98a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a52d095a92fee8b5ef33bed642ef715",
+    "url": "/divar-widget-base/index.html"
+  },
+  {
+    "revision": "04540817216eb84511e2",
+    "url": "/divar-widget-base/static/css/2.8d447336.chunk.css"
+  },
+  {
+    "revision": "eebe65b42d576f828fde",
+    "url": "/divar-widget-base/static/css/main.b82b24d2.chunk.css"
+  },
+  {
+    "revision": "04540817216eb84511e2",
+    "url": "/divar-widget-base/static/js/2.5a4d2783.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/divar-widget-base/static/js/2.5a4d2783.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eebe65b42d576f828fde",
+    "url": "/divar-widget-base/static/js/main.cfcf6ec7.chunk.js"
+  },
+  {
+    "revision": "8398c23ffa0075ae8369",
+    "url": "/divar-widget-base/static/js/runtime-main.9f1f1194.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/divar-widget-base/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/divar-widget-base/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/divar-widget-base/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/divar-widget-base/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/divar-widget-base/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "3f2294642317fad98ff25187e9d45274",
+    "url": "/divar-widget-base/static/media/yekanweb_regular.3f229464.ttf"
+  }
+]);
